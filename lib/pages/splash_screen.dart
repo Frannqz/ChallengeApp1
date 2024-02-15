ecec3a0f82d951628127cae92f403a827d5c9fd5
Francisco González Rodríguez <paco_glez02@hotmail.com>
2024-02-14 22:58:41 -0600

    Splash screen finalizada

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:challenge/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:splash_view/source/presentation/pages/pages.dart';
+import 'package:splash_view/source/presentation/widgets/done.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashView(
+      backgroundColor: Color.fromRGBO(253, 69, 86, 1),
+      logo: Image.asset(
+          "images/valo_logo.png"), // Ruta relativa a la carpeta de assets
+      done: Done(const HomePage(),
+          animationDuration: const Duration(milliseconds: 300)),
+    );
+  }
+}
